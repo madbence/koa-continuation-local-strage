bbf98c71a02d934c8ed6046a6463ea4999210004
Doug Moscrop <Doug.Moscrop@d2l.com>
2016-02-17 20:55:13 -0500

    Use enter/exit instead of bind

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ module.exports = function create(opts) {
   if (!opts.ns) {
     throw new Error('opts.ns is needed!');
   }
-
+  const ns = opts.ns;
   return function* (next) {
-    yield new Promise(opts.ns.bind(function (resolve) {
-      resolve();
-    }));
-    yield* next;
+    const ctx = ns.createContext();
+    ns.enter(ctx);
+    try {
+      yield* next;
+    } finally {
+      ns.exit(ctx);
+    }
   };
 };
